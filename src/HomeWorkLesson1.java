a5db0a3f9cbc570d41e571c2891bad997091000c
AnnaPozina <pozina.qa@gmail.com>
2021-07-20 21:06:55 +0300

    update

---
+++ src/HomeWorkLesson1.java
@@ -0,0 +1,13 @@
+public class HomeWorkLesson1 {
+
+    public static void HomeWork(String[] args){
+     for (int i = 0;i <= 10; i++){
+        System.out.println("JAVA HOME");}
+
+
+    }
+    public static int HomeWork(int a, int b){
+
+     return a;
+    }
+}
